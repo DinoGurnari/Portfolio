aae9e9bc834a7cd990d0a4fea3d3a33e6211c709
DinoGurnari <dinogurnari@gmail.com>
2021-08-06 22:03:33 +0200

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// import * as w3 from "https://www.w3schools.com/lib/w3.js"
+
+// function setCard(cardId) {
+//     var cardList = document.getElementsByClassName(".carte");
+//     cardList.forEach(card => {
+//         w3.hide(card);
+//     });
+//     w3.show(document.getElementById('#about'));
+// }
